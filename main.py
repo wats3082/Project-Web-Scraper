263be4d8965c3cf671328b81193358baca01a226
wats3082 <92612217+wats3082@users.noreply.github.com>
2021-12-19 12:03:19 -0700

    git push

--- main.py
+++ main.py
@@ -3,31 +3,21 @@ import requests  # this module helps us to download a web page
 
 
 
+url="http://www.ibm.com"
+data = requests.get(url).text
+soup= BeautifulSoup(data, "html.parser")
 
+#scrape all links from the page
+for link in soup.find_all('a', href=True):
+    print(link.get('href'))
 
-# this is the stored info from html get request stored in variable
-html_info= "<!DOCTYPE html>\
-            <html>\
-            <head>\
-            <title>Page Title</title>\
-            </head>\
-            <body>\
-            <h3><b id='boldest'>Lebron James</b></h3>\
-            <p> Salary: $ 92,000,000 </p>\
-            <h3> Stephen Curry</h3>\
-            <p> Salary: $85,000, 000 </p>\
-            <h3> Kevin Durant </h3>\
-            <p> Salary: $73,200, 000</p>\
-            </body>\
-            </html>"
+#scrape all image tags
+#for link in soup.find_all('img'):  # in html image is represented by the tag <img>
+ #   print(link)
+  #  print(link.get('src'))
 
-# next create a beautiful soup object  which represents the document as a nested data structure
-soup = BeautifulSoup(html_info, "html.parser")
-
-#First, the document is converted to Unicode, (similar to ASCII), \
-# and HTML entities are converted to Unicode characters. Beautiful Soup transforms a complex HTML document
-# into a complex tree of Python objects. The BeautifulSoup object can create other types of objects.
-#We can use the method prettify() to display the HTML in the nested structure:
-
-print(soup.prettify())
-
+#scrape data from HTML tables, this is a website about color codes
+urlColor = "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBM-DA0321EN-\
+SkillsNetwork/labs/datasets/HTMLColorCodes.html"
+#get contents and save as test
+dataColor = requests.get(urlColor).text
